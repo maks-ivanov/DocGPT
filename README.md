c1b2ae7a6f1cb2b6a129cc61c78f963327b3f0ec
myusername <myemail@example.com>
2023-04-15 13:54:59 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DocGPT
-Simple utility for chatting with documentation
+Simple utility for chatting with documentation. Create a .env file with the following variables:
+
+```OPENAI_API_KEY=your_api_key```
---
+++ main.py
@@ -0,0 +1,63 @@
+import argparse
+import traceback
+
+from dotenv import load_dotenv
+from langchain import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import WebBaseLoader
+
+# argument parsing logic
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.memory import ConversationBufferMemory
+
+load_dotenv()
+parser = argparse.ArgumentParser(description="Configure documentation chat system")
+parser.add_argument("--url", "-u", type=str, help="URL of the documentation")
+parser.add_argument(
+    "--model",
+    "-m",
+    type=str,
+    help="LLM to use",
+    default="gpt-3.5-turbo",
+    choices=["gpt-3.5-turbo", "gpt-4"],
+)
+parser.add_argument(
+    "--temperature", "-t", type=float, help="temperature of the model", default=0.7
+)
+parser.add_argument(
+    "--verbose", "-v", action="store_true", help="increase output verbosity"
+)
+
+args = parser.parse_args()
+
+# load the documentation
+loader = WebBaseLoader(args.url)
+docs = loader.load()
+llm = ChatOpenAI(streaming=True, temperature=0, model=args.model)
+vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
+
+chat_history = []
+
+chain = ConversationalRetrievalChain.from_llm(
+    llm=llm,
+    retriever=vector_store.as_retriever(),
+    return_source_documents=args.verbose,
+)
+
+# chat loop
+print(f"Starting chat session for {args.url} using {args.model} with t={args.temperature}...")
+while True:
+    try:
+        query = input("You: ")
+        result = chain({"question": query, "chat_history": chat_history})
+        answer = result["answer"]
+        print(f"DocGPT: {answer}")
+        chat_history += [(query, answer)]
+    except KeyboardInterrupt:
+        print("Ending chat session...")
+        break
+    except Exception as e:
+        breakpoint()
+        tb = traceback.format_exc()
+        print("Oops:", e, tb)
